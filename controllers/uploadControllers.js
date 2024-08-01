6afd0589a3f1190271909b3c31514c7b9550993e
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-08-01 10:48:26 +0100

    uploadImage - store img metadata

--- controllers/uploadControllers.js
+++ controllers/uploadControllers.js
@@ -1,3 +1,25 @@
-const uploadImage = (req, res) => {};
+const uploadImage = (req, res) => {
+  try {
+    const fileData = req.file;
+
+    if (!fileData) {
+      return res.json({
+        error: "No file uploaded",
+      });
+    }
+
+    const responseObj = {
+      name: fileData.originalname,
+      type: fileData.mimetype,
+      size: fileData.size,
+    };
+
+    return res.json(responseObj);
+  } catch (error) {
+    return res.json({
+      Error: "Internal server error",
+    });
+  }
+};
 
 module.exports = { uploadImage };
