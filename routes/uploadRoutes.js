5e96f8d8b5bf8492dbcec926c4321db42acf2e9a
Jake Samuels <jakesamuels@Jakes-MacBook-Pro.local>
2024-08-01 10:48:47 +0100

    uploadImage route - POST

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { uploadImage } = require("./../controllers/uploadControllers");
+const multer = require("multer");
+const upload = multer({ dest: "fileanalyse/" });
 
 const router = express.Router();
 
-router.route("/").post(uploadImage);
+router.route("/").post(upload.single("upfile"), uploadImage);
 
 module.exports = router;
